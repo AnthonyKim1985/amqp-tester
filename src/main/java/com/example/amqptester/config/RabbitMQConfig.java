87a4869d601534b57cca297a3715949780405bb6
김진혁 <jinhyuk.kim@albamapp.com>
2018-07-02 18:32:01 +0900

    Apply RabbitTemplate to Jackson2JsonMessageConverter

--- src/main/java/com/example/amqptester/config/RabbitMQConfig.java
+++ src/main/java/com/example/amqptester/config/RabbitMQConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,13 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
     public static final String QueueName = "amqp-tester";
     public static final String SecondaryQueueName = "amqp-tester-2";
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
 
     @Bean
     public Queue queue() {
